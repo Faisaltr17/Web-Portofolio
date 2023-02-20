6fb24eb2c97f44a5703bf21bc7c9946973b282f8
Muhammad Faisal <muhammadfaisaltr3@gmail.com>
2023-02-21 00:38:35 +0700

    web porto update

--- main.js
+++ main.js
@@ -12,3 +12,30 @@ function toggleDetail(e){
     const detail = $(item).children(".journey-exp-item-detail")
     $(detail).slideToggle()
 }
+
+function showMore(e) {
+    const target=$(e.target)
+    const moreBtn = document.querySelector('.showMore');
+
+    // Index 0 is already displaying so start from 1.
+    let currentIndex = 6;
+    
+    moreBtn.addEventListener('click', (e) => {
+      const demos = [...document.querySelectorAll('.journey-exp-item')];
+      
+      for (let i = currentIndex; i < currentIndex + 3; i++) {
+        if (demos[i]) {
+            demos[i].style.display = 'block';
+                 
+        }
+      }
+      currentIndex += 3;
+      
+      
+      // If you display all the elements then hide the show more button
+      if (currentIndex >= demos.length) {
+        $(target).html("Less Experience").addClass("active")                                  
+      }
+      
+    })                                    
+}
